e60a4f10e220da9714d5132d6963316d8a2bc5ed
Fabrice ILBOUDO <donilboudo@users.noreply.github.com>
2013-09-23 15:30:05 -0300

    update

--- example/exemple.dart
+++ example/exemple.dart
@@ -3,8 +3,9 @@ import 'package:arraySort/as.dart';
 void main() {
  
  List list = [5,1,9,7,0,12,6];
- var sortlist = sort(list);
+ var sortlist1 = sort1(list);
  print("La liste avant tri est ${list}");
- 
  print("La liste apres un tri par ordre croissant est ${sortlist}");
+ var sortlist2 = sort2(list);
+ print("La liste apres un tri par ordre croissant est ${sortlist2}");
 }
