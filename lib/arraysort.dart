f33208403126056da206ec7b72eac709f2a07c01
Fabrice ILBOUDO <donilboudo@users.noreply.github.com>
2013-09-23 15:31:15 -0300

    update

--- lib/arraysort.dart
+++ lib/arraysort.dart
@@ -1,11 +1,25 @@
 part of as;
 
-List sort(List list){
+List sort1(List list){
 for(var i=0;i<list.length;i++){
   
   for(var j=0;j<list.length;j++){
     var permut = list[i];
     if(list[i]<list[j]){
+      list[i]=list[j];
+      list[j]=permut;
+    }
+  }
+}
+return list;
+}
+
+List sort2(List list){
+for(var i=0;i<list.length;i++){
+  
+  for(var j=0;j<list.length;j++){
+    var permut = list[i];
+    if(list[i]>list[j]){
       list[i]=list[j];
       list[j]=permut;
     }
